a8dc28ebc94980477619fa0ea54cfe1a1111b13d
SergeyLobodin <lobodin01@mail.ru>
2023-08-14 13:33:22 +0300

    edited_3 contact_us link

--- docs/js/hamburger.js
+++ docs/js/hamburger.js
@@ -1,14 +1,15 @@
 const hamburger = document.querySelector(".hamburger");
 const navbar = document.querySelector(".navbar__lists");
-const oldLink = document.getElementById("change");
-const newMobileLink = `<a href="./contact us.html" class="navbar__link">Contact us</a>`;
 
-if (window.innerWidth <= 575) {
-  console.log(window.innerWidth);
-  oldLink.innerHTML = newMobileLink;
-}
+const contactUs = document.querySelector(".btn__contact");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navbar.classList.toggle("dropdown");
 });
+
+if (window.innerWidth <= 575) {
+  contactUs.classList.remove("btn__contact");
+  contactUs.classList.remove("btn");
+  contactUs.classList.add("navbar__link");
+}
